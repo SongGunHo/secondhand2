a6b8e042a311b8823780f9e0c81700ca1fd65946
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-06-18 12:34:13 +0900

    test 에러 코드 수정중

--- src/test/java/org/koreait/survey/diabetes/sevices/DiabetesSurveyPredictServiceTest.java
+++ src/test/java/org/koreait/survey/diabetes/sevices/DiabetesSurveyPredictServiceTest.java
@@ -20,17 +20,18 @@ public class DiabetesSurveyPredictServiceTest {
 
 
     @Test
-    void test(){}
-        String json ="[[Female, 65.0, 0, 1, 4, 42.88, 5.0, 130], [Male, 47.0, 0, 0, 0, 27.32, 5.8, 145], [Female, 56.0, 1, 0, 1, 21.31, 4.5, 145], [Male, 26.0, 0, 0, 0, 23.38, 6.5, 90], [Female, 54.0, 0, 0, 1, 20.45, 6.1, 140]]";
+    void test()throws Exception {
+        String json = "[[Female, 65.0, 0, 1, 4, 42.88, 5.0, 130], [Male, 47.0, 0, 0, 0, 27.32, 5.8, 145], [Female, 56.0, 1, 0, 1, 21.31, 4.5, 145], [Male, 26.0, 0, 0, 0, 23.38, 6.5, 90], [Female, 54.0, 0, 0, 1, 20.45, 6.1, 140]]";
 
-        List<List<Member>> items= om.readValue(json, new TypeReference<>(){});
+        List<List<Member>> items = om.readValue(json, new TypeReference<>() {});
         List<Integer> result = serviceTest.process(items);
-
+    }
     @Test
     void test2(){
-        List<Number> item = List.of(1,46,0,0.0,0.0,3,29.6,5.8,130);
-
-        boolean result = serviceTest.isD
+        //List<Number> item = List.of(1, 46.0, 0.0, 0.0, 3, 29.6, 5.8, 130);
+        List<Number> item = List.of(1, 41, 0, 0, 0,29.6, 8.2, 126);
+        boolean result = serviceTest.isDiabetes(item);
+        System.out.println(result);
     }
 
 }
