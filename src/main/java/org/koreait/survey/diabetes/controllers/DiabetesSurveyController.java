b23913ba56191519a710859a5c603c128d46445d
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-06-18 13:17:28 +0900

    csontroller 공통 함수 생성

--- src/main/java/org/koreait/survey/diabetes/controllers/DiabetesSurveyController.java
+++ src/main/java/org/koreait/survey/diabetes/controllers/DiabetesSurveyController.java
@@ -4,25 +4,41 @@ import jakarta.validation.Valid;
 import org.flywaydb.core.api.callback.Error;
 import org.koreait.global.libs.Utils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.imageio.stream.ImageOutputStreamImpl;
+import java.util.List;
 
 @Controller
 @RequestMapping("/survey/diabetes")
 @SessionAttributes("requestDiabetesSurvey")
 public class DiabetesSurveyController {
-    private final Utils utils;
+    private  Utils utils;
+
+    @ModelAttribute("addCss")
+    public List<String> addCss(){
+        return List.of("survey/diabetes/style");
+    }
+
+
+
+
     @ModelAttribute("requestDiabetesSurvey")
     public RequestDiabetesSurvey requestDiabetesSurvey(){
 
         return new RequestDiabetesSurvey();
     }
     @GetMapping("/step1")
-    public String step1(@ModelAttribute RequestDiabetesSurvey form){
+    public String step1(@ModelAttribute RequestDiabetesSurvey form, Model model){
+        commonProcess("step", model);
         return utils.tpl("survey/diabetes/step1");
     }
     @GetMapping("/step2")
-    public String step2 (RequestDiabetesSurvey form , Error errors){
+    public String step2 (RequestDiabetesSurvey form , Error errors, Model model){
+        commonProcess("step",model);
         if (errors.hasErrors()){
             return utils.tpl("survey/diabetes/step1");
         }
@@ -30,7 +46,8 @@ public class DiabetesSurveyController {
     }
 
     @PostMapping("/process")
-    public String process(@Valid RequestDiabetesSurvey form , Errors errors){
+    public String process(@Valid RequestDiabetesSurvey form , Errors errors ,Model model){
+        commonProcess("step", model);
 
         if(errors.hasErrors()){
             return utils.tpl("survey/diabetes/step2");
@@ -45,9 +62,29 @@ public class DiabetesSurveyController {
      */
 
     @GetMapping("/result/{seq}")
-    public String result(@PathVariable("seq") Long seq){
+    public String result(@PathVariable("seq") Long seq, Model model){
+        commonProcess("result", model);
         return utils.tpl("survey/diabetes/result");
 
+    }
+
+    /**
+     * controller 공통 처리
+     * @param mode
+     * @param model
+     */
+
+    private void commonProcess(String mode , Model model){
+        mode = StringUtils.hasText(mode) ? mode : "step";
+        String pageTittle ="";
+        if(mode.equals("step")){
+            pageTittle =  utils.getMessage("당뇨_고위험군_테스트");
+
+        }else if(mode.equals("result")){
+            pageTittle = utils.getMessage("당뇨_고위험군_결과");
+
+        }
+        model.addAttribute("pageTitle", pageTittle);
     }
 
 
