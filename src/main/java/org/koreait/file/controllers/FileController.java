dd60f7325ba8b886bbccaa2804238341ab30c1dc
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-07-11 16:50:47 +0900

    ㅇ

---
+++ src/main/java/org/koreait/file/controllers/FileController.java
@@ -0,0 +1,11 @@
+package org.koreait.file.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+
+public class FileController {
+}
--- src/test/java/org/koreait/newtrendservice/NewTrendServiceText.java
+++ src/test/java/org/koreait/newtrendservice/NewTrendServiceText.java
@@ -1,15 +1,15 @@
-package org.koreait.newtrendservice;
-
-import org.koreait.trend.services.NewsTrendService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class NewTrendServiceText {
-
-    @Autowired
-    private NewsTrendService service;
-
-
-
-}
+//package org.koreait.newtrendservice;
+//
+//import org.koreait.trend.services.NewsTrendService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class NewTrendServiceText {
+//
+//    @Autowired
+//    private NewsTrendService service;
+//
+//
+//
+//}
