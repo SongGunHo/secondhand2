c8e06f4423d1adcdf6fbb0b729a25ff02fbcea8b
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-07-14 01:09:49 +0900

    카카오 키값 변경

--- src/main/java/org/koreait/product/repositories/ProductRepository.java
+++ src/main/java/org/koreait/product/repositories/ProductRepository.java
@@ -2,6 +2,9 @@ package org.koreait.product.repositories;
 
 import org.koreait.product.entities.Product;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface ProductRepository extends ListCrudRepository<Product, Long> {
 }
