b3d032e6ecb0fd5e5009bef3025d3228df6e92f1
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-07-14 16:24:42 +0900

    다시 만드는 중

--- src/main/java/org/koreait/file/controllers/FileController.java
+++ src/main/java/org/koreait/file/controllers/FileController.java
@@ -1,36 +1,36 @@
-package org.koreait.file.controllers;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@Operation(summary = "파일 업로드 처리", method = "MULTIPART")
-@ApiResponse(responseCode = "201", description = "파일 업로드 성공시 파일 목록을 출력 한다")
-@PostMapping("upload")
-@RequiredArgsConstructor
-@Tag(name = "파일 api", description = "파일 업로드 다운로드 조회기능을 제공")
-@RequestMapping({"/api/file", "/file"})
-public class FileController {
-
-
-    @Operation(summary = "파일 등록 번호호 파일 정보 한개 조회")
-    @Parameters({
-            @Parameter(name = "seq", in=)
-    })
-
-
-
-
-
-
-
-
-
-}
+//package org.koreait.file.controllers;
+//
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.Parameter;
+//import io.swagger.v3.oas.annotations.Parameters;
+//import io.swagger.v3.oas.annotations.responses.ApiResponse;
+//import io.swagger.v3.oas.annotations.tags.Tag;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@Operation(summary = "파일 업로드 처리", method = "MULTIPART")
+//@ApiResponse(responseCode = "201", description = "파일 업로드 성공시 파일 목록을 출력 한다")
+//@PostMapping("upload")
+//@RequiredArgsConstructor
+//@Tag(name = "파일 api", description = "파일 업로드 다운로드 조회기능을 제공")
+//@RequestMapping({"/api/file", "/file"})
+//public class FileController {
+//
+//
+//    @Operation(summary = "파일 등록 번호호 파일 정보 한개 조회")
+//    @Parameters({
+//            @Parameter(name = "seq", in=)
+//    })
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//}
