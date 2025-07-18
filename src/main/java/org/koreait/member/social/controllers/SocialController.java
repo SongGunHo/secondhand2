88bc2edc1bb8d0e231c22b089ebb230029bf457c
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-07-18 13:21:23 +0900

    작업중

--- src/main/java/org/koreait/member/social/controllers/SocialController.java
+++ src/main/java/org/koreait/member/social/controllers/SocialController.java
@@ -5,6 +5,7 @@ import org.koreait.global.annotations.ApplyCommonController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @ApplyCommonController
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/member/social")
 public class SocialController {
 
-    @GetMapping("/")
-    public String callback(){
+    @GetMapping("/callback/kakao")
+    public String callback(@RequestParam("code") String code){
+
+        return null;
 
     }
 
