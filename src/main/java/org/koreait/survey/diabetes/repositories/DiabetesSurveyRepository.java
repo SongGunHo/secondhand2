0350da9f7e3f4eb406ad242bcc8a7678ff4795d3
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-07-20 20:05:58 +0900

    카카오 로그인 버튼 만들기

--- src/main/java/org/koreait/survey/diabetes/repositories/DiabetesSurveyRepository.java
+++ src/main/java/org/koreait/survey/diabetes/repositories/DiabetesSurveyRepository.java
@@ -3,7 +3,9 @@ package org.koreait.survey.diabetes.repositories;
 import org.koreait.survey.diabetes.entities.DiabetesSurvey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository("DiabetesSurveyRepositoryv1")
 public interface DiabetesSurveyRepository extends JpaRepository<DiabetesSurvey, Long>, QuerydslPredicateExecutor<DiabetesSurvey> {
 
 }
--- src/main/java/org/koreait/survey/repository/DiabetesSurveyRepository.java
+++ src/main/java/org/koreait/survey/repository/DiabetesSurveyRepository.java
@@ -1,7 +1,9 @@
 package org.koreait.survey.repository;
 
 import org.koreait.survey.diabetes.entities.DiabetesSurvey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.data.repository.ListCrudRepository;
 
-public interface DiabetesSurveyRepository extends ListCrudRepository<DiabetesSurvey , Long> {
+public interface DiabetesSurveyRepository extends JpaRepository<DiabetesSurvey, Long> {
 }
